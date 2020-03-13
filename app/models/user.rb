c29fd0110949e48e1c3d4a9a4863bac7425c078d
stevenks17 <stevenks17@gmail.com>
2020-03-13 14:00:04 -0400

    8th commit(Model Relationships)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reviews
+  has_many :reviewed_teas, through: :reviews, source: :tea 
+  has_many :teas #user creates this tea
+
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

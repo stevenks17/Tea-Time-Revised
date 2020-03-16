faf2755c63a99db450a1f1d1d3bb351d5cbc7c3e
stevenks17 <stevenks17@gmail.com>
2020-03-16 10:19:23 -0400

    8th commit(OmniAuth GitHub implemented)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,17 @@ class RegistrationsController < Devise::RegistrationsController
     private
 
     def sign_up_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit( :name
+                                      :email, 
+                                      :password, 
+                                      :password_confirmation)
     end
 
     def account_update_params
-        params.require(:user).permit(:password, :password_confirmation, :current_password)
+        params.require(:user).permit(  :name,
+                                       :email
+                                       :password, 
+                                       :password_confirmation, 
+                                       :current_password)
     end
-    
-    
-
 end

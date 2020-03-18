050451027c0e514a39cacc8c030c137e6a53434a
github username <github email address>
2020-03-18 12:19:10 -0400

    11th commit( Tea Model/Controller/Views wired)

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -3,7 +3,7 @@ class Tea < ApplicationRecord
   belongs_to :user
   has_many :reviews
   has_many :users, through: :reviews
-
+  accepts_nested_attributes_for :brand
   validates :flavor, presence:true  
   validate :not_a_duplicate
 
